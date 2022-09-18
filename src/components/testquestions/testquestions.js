19b4b026ae68aac75c41a20aaa6ca3e8af53be81
Abhay Chandavar <abhaychandavar11@gmail.com>
2022-09-18 16:38:09 +0530

    adding rado for options

--- src/components/testquestions/testquestions.js
+++ src/components/testquestions/testquestions.js
@@ -1,17 +1,51 @@
-import react from "react";
-import "./testquestions.css"
-const TestQuestions = ({question,options,qnum}) => {
-   
-    return (
-        <div>
-            <div >{qnum+1} {question}</div>
-            <div ><input type="radio"/>{options[0]}</div>
-            <div ><input type="radio"/>{options[1]}</div>
-            <div ><input type="radio"/>{options[2]}</div>
-            <div ><input type="radio"/>{options[3]}</div>
-        </div>
-         
-
-    );
-} 
+import react from 'react';
+import './testquestions.css';
+const TestQuestions = ({ question, questionId, options, qnum, onChange }) => {
+  return (
+    <div>
+      <div>
+        {qnum + 1} {question}
+      </div>
+      <div>
+        <input
+          type="radio"
+          name={options[0]+qnum}
+          value={0}
+          onChange={(event) => {
+            console.log("changed")
+            onChange(questionId, event.target.value);
+          }}
+        />
+        {options[0]}
+        <input
+          type="radio"
+          name={options[1]+qnum}
+          value={1}
+          onChange={(event) => {
+            onChange(questionId, event.target.value);
+          }}
+        />
+        {options[1]}
+        <input
+          type="radio"
+          name={options[2]+qnum}
+          value={2}
+          onChange={(event) => {
+            onChange(questionId, event.target.value);
+          }}
+        />
+        {options[2]}
+        <input
+          type="radio"
+          name={options[3]+qnum}
+          value={3}
+          onChange={(event) => {
+            onChange(questionId, event.target.value);
+          }}
+        />
+        {options[3]}
+      </div>
+    </div>
+  );
+};
 export default TestQuestions;
--- src/pages/quiz questions/questions.js
+++ src/pages/quiz questions/questions.js
@@ -1,61 +1,85 @@
-import React, { useEffect, useState } from "react";
-import "./questions.css";
-import TestQuestions from "../../components/testquestions/testquestions"
-import Cookies from "js-cookie";
-import { useNavigate, useLocation} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import './questions.css';
+import TestQuestions from '../../components/testquestions/testquestions';
+import Cookies from 'js-cookie';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 function Questions() {
-    const {state} = useLocation();
-    const {quizId, quizTitle} = state;
-    const navigate = useNavigate();
-    const [ questions , setquestion ] = useState([]);
-    useEffect(() => {fetchQuestions();},[]);
+  const { state } = useLocation();
+  const { quizId, quizTitle } = state;
+  const navigate = useNavigate();
+  const [questions, setquestion] = useState([]);
+  const [answers, setAnswers] = useState({});
+  useEffect(() => {
+    fetchQuestions();
+  }, []);
+  const handleAnswerChange = (questionId, answer) => setAnswers({[questionId]: answer, ...answers})
+  const handleSubmit = () => {
+    console.log("Answers: ", answers)
+    //navigate('/score');
+  };
 
-    const handleSubmit = () => {
-        navigate('/score');
-   }
-
-    const fetchQuestions = async() => 
-     {
-        console.log("QUIZ ID: ", quizId)
-        try {
-            const token = Cookies.get('token')
-            const header = {
-                method: 'GET',
-                headers:{'Content-Type': 'application/json','authorization': "Bearer " + token  } 
-            };
-            const sol = await fetch(`http://localhost:4000/quizQuestion/${quizId}`,header);
-            if(sol.status===200) {
-              const solRes = await sol.json()
-              console.log("questipons: ", solRes.data)
-              return setquestion(solRes.data);
-            }
-                else {
-               return alert ("question not found")
-            }
-        }
-            catch(err){
-               console.log("errrrrrrr",err);
-               return alert("fail to load");
-            } 
-        }
-    return (
-        <div className="questionscontainer">
-            <div >
-                <h1 className="questionTile">{quizTitle}</h1>
-            </div>
-            <div className="questionbody">
-            {
-                questions.map((ele, idx) => {
-                    return <TestQuestions question={ele.question} options={ele.option} key={idx} qnum={idx} />
-               })
-            }
-            </div>
-            <div className="subbutton">
-                <button onClick={handleSubmit} className="buttonstyle" style={{backgroundColor:"#F68F00", color:'white', boxShadow:'4px 3px #cdb4b4'}} type="button">Submit</button>
-                
-            </div>
-        </div>
-    )
+  const fetchQuestions = async () => {
+    console.log('QUIZ ID: ', quizId);
+    try {
+      const token = Cookies.get('token');
+      const header = {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: 'Bearer ' + token,
+        },
+      };
+      const sol = await fetch(
+        `http://localhost:4000/quizQuestion/${quizId}`,
+        header
+      );
+      if (sol.status === 200) {
+        const solRes = await sol.json();
+        console.log('questipons: ', solRes.data);
+        return setquestion(solRes.data);
+      } else {
+        return alert('question not found');
+      }
+    } catch (err) {
+      console.log('errrrrrrr', err);
+      return alert('fail to load');
+    }
+  };
+  return (
+    <div className="questionscontainer">
+      <div>
+        <h1 className="questionTile">{quizTitle}</h1>
+      </div>
+      <div className="questionbody">
+        {questions.map((ele, idx) => {
+          return (
+            <TestQuestions
+              question={ele.question}
+              options={ele.option}
+              key={idx}
+              qnum={idx}
+              questionId={ele.questionId}
+              onChange={handleAnswerChange}
+            />
+          );
+        })}
+      </div>
+      <div className="subbutton">
+        <button
+          onClick={handleSubmit}
+          className="buttonstyle"
+          style={{
+            backgroundColor: '#F68F00',
+            color: 'white',
+            boxShadow: '4px 3px #cdb4b4',
+          }}
+          type="button"
+        >
+          Submit
+        </button>
+      </div>
+    </div>
+  );
 }
 export default Questions;
